90fbca2503ff01ccf796e3cbbad9516ede74cb33
Peter Yao <peyao@protonmail.ch>
2018-02-22 16:25:23 -0800

    Polish version-select

--- docs/custom/version-select.js
+++ docs/custom/version-select.js
@@ -23,9 +23,8 @@ $(function initializeVersionSelect() {
 
   // `base_url` comes from the base.html template for this theme.
   var REL_BASE_URL = (typeof base_url === 'undefined' ? '.' : base_url);
-  var ABS_BASE_URL = normalizePath(window.location.pathname + "/" +
-                                   REL_BASE_URL);
-  var CURRENT_VERSION = ABS_BASE_URL.split("/v/")[1] || 'latest';
+  var ABS_BASE_URL = normalizePath(window.location.pathname + "/" + REL_BASE_URL);
+  var CURRENT_VERSION_STR = ABS_BASE_URL ? ABS_BASE_URL.split('/v/')[1].split('/')[0] : 'latest';
 
   function makeSelect(options, selected) {
     var select = document.createElement("select");
@@ -41,9 +40,9 @@ $(function initializeVersionSelect() {
 
   function getCurrentVersion(versions) {
     return versions.find(function(i) {
-      return i.version === CURRENT_VERSION ||
-             i.aliases.includes(CURRENT_VERSION) ||
-             (CURRENT_VERSION === 'latest' && i.latest);
+      return i.version === CURRENT_VERSION_STR ||
+             i.aliases.includes(CURRENT_VERSION_STR) ||
+             (CURRENT_VERSION_STR === 'latest' && i.latest);
     });
   }
 
@@ -51,14 +50,15 @@ $(function initializeVersionSelect() {
     var match = window.location.href.match(/\/v\/[0-9]*\.[0-9]*\.[0-9]*\//);
     if (match) {
       // path has version number in it
-      // window.location.href = window.location.href.replace(/\/v\/[0-9]*\.[0-9]*\.[0-9]*\//,  '/v/' + this.value + '/');
       return window.location.href.replace(/\/v\/[0-9]*\.[0-9]*\.[0-9]*\//,  '/v/' + version + '/');
     } else {
       // path does not have version in it
-      // window.location.href = window.location.href.replace(window.location.origin, window.location.origin + '/v/' + this.value);
       return window.location.href.replace(window.location.origin, window.location.origin + '/v/' + version);
     }
-    // window.location.href = REL_BASE_URL + "/../" + this.value;
+  }
+
+  function getLatestVersionLink() {
+    return window.location.href.replace(/\/v\/[0-9]*\.[0-9]*\.[0-9]*\//, '/');
   }
 
   var getVersions = $.get('/versions.json');
@@ -69,6 +69,7 @@ $(function initializeVersionSelect() {
       var versions = versionsResponse[0];
       var template = templateResponse[0];
       var currentVersion = getCurrentVersion(versions);
+      var latestVersion = versions.find(function(version) { return version.latest; });
 
       // add template
       $('.rst-versions').append(template);
@@ -77,10 +78,11 @@ $(function initializeVersionSelect() {
       var $versionSelectBar = $('.version-select__bar');
       var $versionSelectBarVersion = $('.version-select__bar-version');
       var $versionSelectMenu = $('.version-select__menu');
-      var $versionSelectMenuListVersions = $('.version-select__menu-versions');
+      var $versionSelectMenuVersions = $('.version-select__menu-versions');
+      var $versionSelectMenuVersionsLatest = $('.version-select__menu-versions-latest');
 
-      // build template
-      $versionSelectBarVersion.append(currentVersion.title);
+      // append select bar version; use CURRENT_VERSION_STR since it's a string that's either "latest" or "*.*.*"
+      $versionSelectBarVersion.append(CURRENT_VERSION_STR);
 
       // expand/collapse click handler
       $versionSelectBar.click(function(event) {
@@ -91,59 +93,12 @@ $(function initializeVersionSelect() {
         }
       });
 
+      // set link for latest version
+      $versionSelectMenuVersionsLatest.attr('href', getLatestVersionLink());
+
+      // append versions
       versions.forEach(function(version) {
-        $versionSelectMenuListVersions.append('<dd><a href="' + getVersionLink(version.version) + '">v' + version.title + '</a></dd>');
+        $versionSelectMenuVersions.append('<dd><a href="' + getVersionLink(version.version) + '">v' + version.title + '</a></dd>');
       });
     });
-
-  return;
-  var xhr = new XMLHttpRequest();
-  // xhr.open("GET", REL_BASE_URL + "/../versions.json");
-  xhr.open("GET", "/versions.json");
-  xhr.onload = function() {
-    var versions = JSON.parse(this.responseText);
-
-
-
-    var currentVersion = versions.find(function(i) {
-      return i.version === CURRENT_VERSION ||
-             i.aliases.includes(CURRENT_VERSION) ||
-             (CURRENT_VERSION === 'latest' && i.latest);
-    });
-
-    var select = makeSelect(versions.map(function(i) {
-      return {text: i.title + (i.latest ? ' (latest)' : ''), value: i.version};
-    }), currentVersion.version);
-    select.id = "version-selector";
-    select.addEventListener("change", function(event) {
-      var match = window.location.href.match(/\/v\/[0-9]*\.[0-9]*\.[0-9]*\//);
-      if (match) {
-        // path has version number in it
-        window.location.href = window.location.href.replace(/\/v\/[0-9]*\.[0-9]*\.[0-9]*\//,  '/v/' + this.value + '/');
-      } else {
-        // path does not have version in it
-        window.location.href = window.location.href.replace(window.location.origin, window.location.origin + '/v/' + this.value);
-      }
-      // window.location.href = REL_BASE_URL + "/../" + this.value;
-    });
-
-    var div = document.createElement('div');
-    div.className = 'version-selected-heading';
-    div.append('v' + currentVersion.title + (currentVersion.latest ? ' (latest)' : ''));
-    var title = document.querySelector("div.wy-side-nav-search");
-    title.insertBefore(div, title.querySelector(".icon-home").nextSibling);
-
-
-    // place select in div
-    div = document.createElement('div');
-    div.className = 'version-select';
-    div.append('v: ');
-    div.append(currentVersion.title);
-    div.append(document.createElement('span'));
-    div.append(select);
-
-    var versionSection = document.querySelector("div.rst-versions");
-    versionSection.prepend(div);
-  };
-  xhr.send();
 });
